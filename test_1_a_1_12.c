691c8586d6c883803ca7527e158bd37f1d285897
KoiralaSanjeev <koirala-s@gr.m-fielders.com>
2024-09-06 15:07:14 +0900

    コード修正しました。

--- test_1_a_1_12.c
+++ test_1_a_1_12.c
@@ -6,6 +6,9 @@ int main(void) {
 
     for (i = 1; i < 10; i ++){
         for(j = 1; j < 10; j++){
+            if(i >j){
+                continue;
+            }
             printf("%d * %d = %d\n", i, j, i * j);
         }
         
